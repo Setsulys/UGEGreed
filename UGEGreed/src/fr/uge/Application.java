3e1285a6ecfd2a2ab13ded7b736ec461f9f09616
LY-IENG Steven <steven.ly412@gmail.com>
2023-03-15 14:28:01 +0100

    try commit start project

---
+++ UGEGreed/src/fr/uge/Application.java
@@ -0,0 +1,31 @@
+package fr.uge;
+
+import java.net.InetSocketAddress;
+
+public class Application {
+	
+	
+	private static void usage() {
+		System.out.println("Usage :");
+		System.out.println("Application host port adress");
+		System.out.println(" - Root Mode - ");
+		System.out.println("Application host port");
+	}
+	
+	
+	public static void main(String[] args) {
+		if(args.length > 3) {
+			usage();
+			return;
+		}
+		String host = args[0];
+		int port = Integer.valueOf(args[1]);	
+		if(args.length == 3) {
+			int adress = Integer.valueOf(args[2]);
+		}
+		InetSocketAddress serverAddress = new InetSocketAddress(host,port);
+		
+		
+		
+	}
+}
