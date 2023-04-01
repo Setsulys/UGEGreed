831fe4de93f7062ef8e830e9f195c25086d7bfca
LY-IENG Steven <steven.ly412@gmail.com>
2023-04-01 16:31:44 +0200

    some modifications 2

--- UGEGreed/src/fr/uge/greed/Application.java
+++ UGEGreed/src/fr/uge/greed/Application.java
@@ -72,54 +72,6 @@ public class Application {
 		}
 
 	}
-	
-//	public class RouteTable {
-//
-//		private final LinkedHashMap<InetSocketAddress, InetSocketAddress> routeTable = new LinkedHashMap<>();
-//		
-//		/**
-//		 * Met A jour la Table de routage
-//		 * @param newAdress
-//		 * @param route
-//		 */
-//		public void UpdateRouteTable(InetSocketAddress newAdress,InetSocketAddress route) {
-//			//newAdress l'adresse de la node que l'on veut,route l'adresse de la node par laquelle on passe pour aller a newAdress
-//			Objects.requireNonNull(newAdress);
-//			Objects.requireNonNull(route);
-//			routeTable.put(newAdress, route);
-//		}
-//		
-//		
-//		/**
-//		 * Supprime une Application et son chemin (clé valeur) de la table de routage lors de la déconnexion de l'Application
-//		 * @param unlinked
-//		 */
-//		public void DeleteRouteTable(InetSocketAddress unlinked) {
-//			Objects.requireNonNull(unlinked);
-//			routeTable.remove(unlinked);
-//		}
-//		
-//		/**
-//		 * Recuppere le chemin (voisin) par lequel une trame doit passer pour atteindre sa destination
-//		 * renvoi null si il n'y a pas de chemin
-//		 * @param destination
-//		 * 
-//		 * @return InetSocketAddress
-//		 */
-//		public InetSocketAddress get(InetSocketAddress destination) {
-//			Objects.requireNonNull(destination);
-//			return routeTable.get(destination);
-//		}
-//		
-//		/**
-//		 * Affiche Toute la route table
-//		 */
-//		@Override
-//		public String toString() {
-//			return routeTable.entrySet().stream().map(e -> e.getKey() + " : " + e.getValue()).collect(Collectors.joining(",","[","]"));
-//		}
-//		
-//	}
 
 
 	// private final SelectionKey key;
